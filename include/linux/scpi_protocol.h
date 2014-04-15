c3c4d86644bc55f6dbf45d26c91a02bf2861d6d4
Sudeep Holla <sudeep.holla@arm.com>
2014-04-15 16:09:42 +0100

    mailbox: add support for System Control and Power Interface(SCPI) protocol
    This patch add supports for System Control and Power Interface (SCPI)
    Message Protocol used between the Application Cores(AP) and the System
    Control Processor(SCP). The MHU peripheral provides a mechanism for
    inter-processor communication between SCP's M3 processor and AP.
    
    SCP offers control and management of the core/cluster power states,
    various power domain DVFS including the core/cluster, certain system
    clocks configuration, thermal sensors and many others.
    
    This protocol library provides interface for all the client drivers using
    SCPI to make use of the features offered by the SCP.
    
    Signed-off-by: Sudeep Holla <sudeep.holla@arm.com>

---
+++ include/linux/scpi_protocol.h
@@ -0,0 +1,30 @@
+/*
+ * SCPI Message Protocol driver header
+ *
+ * Copyright (C) 2014 ARM Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <linux/types.h>
+
+struct scpi_opp {
+	u32 *freqs;
+	u32 latency; /* in usecs */
+	int count;
+};
+
+unsigned long scpi_clk_get_val(u16 clk_id);
+int scpi_clk_set_val(u16 clk_id, unsigned long rate);
+int scpi_dvfs_get_idx(u8 domain);
+int scpi_dvfs_set_idx(u8 domain, u8 idx);
+struct scpi_opp *scpi_dvfs_get_opps(u8 domain);
