b040138ca67320957703345225de4ecb96f4c6b5
Punit Agrawal <punit.agrawal@arm.com>
2014-06-02 18:21:07 +0100

    thermal: scpi: Add support for cluster cooling devices
    Extend the SCPI sensor driver to register cluster cooling devices.
    
    Signed-off-by: Punit Agrawal <punit.agrawal@arm.com>
    Signed-off-by: Jon Medhurst <tixy@linaro.org>

--- drivers/thermal/scpi-thermal.c
+++ drivers/thermal/scpi-thermal.c
@@ -1,3 +1,6 @@
+#include <linux/cpu_cooling.h>
+#include <linux/cpufreq.h>
+#include <linux/cpumask.h>
 #include <linux/module.h>
 #include <linux/platform_device.h>
 #include <linux/scpi_protocol.h>
@@ -6,9 +9,13 @@
 
 #define SOC_SENSOR "SENSOR_TEMP_SOC"
 
+#define NUM_CLUSTERS 2
+
 struct scpi_sensor {
 	u16 sensor_id;
 	struct thermal_zone_device *tzd;
+	struct cpumask cluster[NUM_CLUSTERS];
+	struct thermal_cooling_device *cdevs[NUM_CLUSTERS];
 };
 static struct scpi_sensor scpi_temp_sensor;
 
@@ -35,13 +42,52 @@ static struct thermal_zone_of_device_ops scpi_of_ops = {
 static int scpi_thermal_probe(struct platform_device *pdev)
 {
 	struct scpi_sensor *sensor_data = &scpi_temp_sensor;
-	int sensor;
+	struct device_node *np;
+	int sensor, cpu;
+	int i, j;
 
 	scpi_ops = get_scpi_ops();
 	if (!scpi_ops)
 		return -EPROBE_DEFER;
 
+	if (!cpufreq_frequency_get_table(0))
+		return -EPROBE_DEFER;
+
 	platform_set_drvdata(pdev, sensor_data);
+
+	for_each_possible_cpu(cpu) {
+		int cluster_id = topology_physical_package_id(cpu);
+		if (cluster_id > NUM_CLUSTERS) {
+			dev_warn(&pdev->dev, "Cluster id: %d > %d\n",
+				 cluster_id, NUM_CLUSTERS);
+			goto error;
+		}
+
+		cpumask_set_cpu(cpu, &sensor_data->cluster[cluster_id]);
+	}
+
+	for (i = 0, j = 0; i < NUM_CLUSTERS; i++) {
+		char node[16];
+
+		snprintf(node, 16, "cluster%d", i);
+		np = of_find_node_by_name(NULL, node);
+
+		if (!np) {
+			dev_info(&pdev->dev, "Node not found: %s\n", node);
+			continue;
+		}
+
+		sensor_data->cdevs[j] =
+			of_cpufreq_cooling_register(np,
+						&sensor_data->cluster[i]);
+
+		if (IS_ERR(sensor_data->cdevs[i])) {
+			dev_warn(&pdev->dev,
+				"Error registering cooling device: %s\n", node);
+			continue;
+		}
+		j++;
+	}
 
 	if ((sensor = scpi_ops->sensor_get_id(SOC_SENSOR)) < 0) {
 		dev_warn(&pdev->dev, "%s not found. ret=%d\n", SOC_SENSOR, sensor);
@@ -60,8 +106,6 @@ static int scpi_thermal_probe(struct platform_device *pdev)
 		dev_warn(&pdev->dev, "Error registering sensor: %ld\n", PTR_ERR(sensor_data->tzd));
 		return PTR_ERR(sensor_data->tzd);
 	}
-
-	thermal_zone_device_update(sensor_data->tzd);
 
 	return 0;
 
