48e3f37aee60019d4c65abc21c4188de08baef69
Mauro Ribeiro <mauro.ribeiro@hardkernel.com>
2014-08-14 16:06:59 -0300

    cpufreq/mp: don't apply QoS here

--- drivers/cpufreq/exynos-mp-cpufreq.c
+++ drivers/cpufreq/exynos-mp-cpufreq.c
@@ -688,15 +688,15 @@ static int exynos_target(struct cpufreq_policy *policy,
 		BUG_ON(freqs[cur]->old != exynos_getspeed(policy->cpu));
 	}
 
-	if (cur == CA15) {
+/*	if (cur == CA15) {
 		target_freq = max((unsigned int)pm_qos_request(PM_QOS_CPU_FREQ_MIN), target_freq);
 		target_freq = min((unsigned int)pm_qos_request(PM_QOS_CPU_FREQ_MAX), target_freq);
-		target_freq = min(g_clamp_cpufreqs[CA15], target_freq); /* add IPA clamp */
+		target_freq = min(g_clamp_cpufreqs[CA15], target_freq); 
 	} else {
 		target_freq = max((unsigned int)pm_qos_request(PM_QOS_KFC_FREQ_MIN), target_freq);
 		target_freq = min((unsigned int)pm_qos_request(PM_QOS_KFC_FREQ_MAX), target_freq);
-		target_freq = min(g_clamp_cpufreqs[CA7], target_freq); /* add IPA clamp */
-	}
+		target_freq = min(g_clamp_cpufreqs[CA7], target_freq); 
+	} */
 
 #ifdef CONFIG_CPU_THERMAL_IPA_DEBUG
 	trace_printk("IPA:%s:%d will apply %d ", __PRETTY_FUNCTION__, __LINE__, target_freq);
@@ -1821,6 +1821,7 @@ static int __init exynos_cpufreq_init(void)
 		pr_err("%s: No set_freq function (ERR)\n", __func__);
 		goto err_set_freq;
 	}
+
 
 	freq_max[CA15] = exynos_info[CA15]->
 		freq_table[exynos_info[CA15]->max_support_idx].frequency;
