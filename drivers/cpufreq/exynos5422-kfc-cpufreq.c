e6e20afca66fdd692d505bb112f9e4edda772a5c
Mauro Ribeiro <mauro.ribeiro@hardkernel.com>
2014-08-14 16:06:18 -0300

    cpufreq/exynos5422(a7): allow min/max frequency scalling

--- drivers/cpufreq/exynos5422-kfc-cpufreq.c
+++ drivers/cpufreq/exynos5422-kfc-cpufreq.c
@@ -409,12 +409,12 @@ static void __init set_volt_table_CA7(void)
 	if(exynos5422_tbl_ver_is_bin2())
 		max_support_idx_CA7 = L3;
 	else
-		max_support_idx_CA7 = L3;
+		max_support_idx_CA7 = L1;
 		
 	if (PKG_ID_DVFS_VERSION == 0x02)
-		min_support_idx_CA7 = L11;
+		min_support_idx_CA7 = L14;
 	else
-		min_support_idx_CA7 = L8;
+		min_support_idx_CA7 = L14;
 }
 
 static bool exynos5422_is_alive_CA7(void)
