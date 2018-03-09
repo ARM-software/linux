b74996c629f510c57866c9a11e1f04de161d6203
Uma Shankar <uma.shankar@intel.com>
2018-03-09 23:47:44 +0530

    drm: Add Plane CTM property
    Add a blob property for plane CSC usage.
    
    v2: Rebase
    
    v3: Fixed Sean, Paul's review comments. Moved the property from
    mode_config to drm_plane. Created a helper function to instantiate
    these properties and removed from drm_mode_create_standard_properties
    Added property documentation as suggested by Daniel, Vetter.
    
    Change-Id: Ic5ecada0ae3c6ecc87fa7e99204abe933513c946
    Signed-off-by: Uma Shankar <uma.shankar@intel.com>
    Signed-off-by: Liviu Dudau <liviu.dudau@arm.com>

--- drivers/gpu/drm/drm_atomic_helper.c
+++ drivers/gpu/drm/drm_atomic_helper.c
@@ -3617,6 +3617,8 @@ void __drm_atomic_helper_plane_duplicate_state(struct drm_plane *plane,
 
 	if (state->degamma_lut)
 		drm_property_blob_get(state->degamma_lut);
+	if (state->ctm)
+		drm_property_blob_get(state->ctm);
 	state->color_mgmt_changed = false;
 }
 EXPORT_SYMBOL(__drm_atomic_helper_plane_duplicate_state);
@@ -3665,6 +3667,9 @@ void __drm_atomic_helper_plane_destroy_state(struct drm_plane_state *state)
 
 	if (state->degamma_lut)
 		drm_property_blob_put(state->degamma_lut);
+
+	if (state->ctm)
+		drm_property_blob_put(state->ctm);
 }
 EXPORT_SYMBOL(__drm_atomic_helper_plane_destroy_state);
 
