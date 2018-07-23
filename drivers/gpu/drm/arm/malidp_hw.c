92ba9c27840cae9292ffd6a18cf35a7617deb634
Alexandru Gheorghe <alexandru-cosmin.gheorghe@arm.com>
2018-07-23 13:17:51 +0100

    drm: mali-dp: Enable mali specific buffer formats
    Enable the following formats
     - DRM_FORMAT_XYUV8888
     - DRM_FORMAT_XVYU2101010
     - DRM_FORMAT_X0L0
     - DRM_FORMAT_X0L2
     - DRM_FORMAT_P010
    
    All formats respect the rules checked by core framebuffer_check except
    DRM_FORMAT_X0L0 and DRM_FORMAT_X0L2 for which we neeed to take into
    consideration that it's a 2x2 tiled format, so the following things
    need special handling:
    
    1) PITCH: needs to cover two rows.
    2) GEM_SIZE: the core formula (drm_gem_fb_create_with_funcs) that
    checks min_object size doesn't work anymore, so I added special check
    in driver for X0L0 and X0L2.
    3) SOURCE_CROPPING: drm_fb_cma_get_gem_addr doesn't properly retrieves
    start address, so I added the right formula for DRM_FORMAT_X0L0 and
    DRM_FORMAT_X0L2 inside the driver.
    
    Signed-off-by: Alexandru Gheorghe <alexandru-cosmin.gheorghe@arm.com>

--- drivers/gpu/drm/arm/malidp_hw.c
+++ drivers/gpu/drm/arm/malidp_hw.c
@@ -74,13 +74,22 @@ static const struct malidp_format_id malidp500_de_formats[] = {
 	{ DRM_FORMAT_ABGR1555, DE_VIDEO1 | DE_GRAPHICS1 | DE_VIDEO2, MALIDP_ID(4, 1) }, \
 	{ DRM_FORMAT_RGB565, DE_VIDEO1 | DE_GRAPHICS1 | DE_VIDEO2, MALIDP_ID(4, 2) }, \
 	{ DRM_FORMAT_BGR565, DE_VIDEO1 | DE_GRAPHICS1 | DE_VIDEO2, MALIDP_ID(4, 3) }, \
+	{ DRM_FORMAT_XYUV8888, DE_VIDEO1 | DE_VIDEO2, MALIDP_ID(5, 0) }, \
 	{ DRM_FORMAT_YUYV, DE_VIDEO1 | DE_VIDEO2, MALIDP_ID(5, 2) },	\
 	{ DRM_FORMAT_UYVY, DE_VIDEO1 | DE_VIDEO2, MALIDP_ID(5, 3) },	\
 	{ DRM_FORMAT_NV12, DE_VIDEO1 | DE_VIDEO2 | SE_MEMWRITE, MALIDP_ID(5, 6) },	\
-	{ DRM_FORMAT_YUV420, DE_VIDEO1 | DE_VIDEO2, MALIDP_ID(5, 7) }
+	{ DRM_FORMAT_YUV420, DE_VIDEO1 | DE_VIDEO2, MALIDP_ID(5, 7) }, \
+	{ DRM_FORMAT_XVYU2101010, DE_VIDEO1 | DE_VIDEO2, MALIDP_ID(6, 0)}, \
+	{ DRM_FORMAT_X0L2, DE_VIDEO1 | DE_VIDEO2, MALIDP_ID(6, 6)}, \
+	{ DRM_FORMAT_P010, DE_VIDEO1 | DE_VIDEO2, MALIDP_ID(6, 7)}
 
 static const struct malidp_format_id malidp550_de_formats[] = {
 	MALIDP_COMMON_FORMATS,
+};
+
+static const struct malidp_format_id malidp650_de_formats[] = {
+	MALIDP_COMMON_FORMATS,
+	{ DRM_FORMAT_X0L0, DE_VIDEO1 | DE_VIDEO2, MALIDP_ID(5, 4)},
 };
 
 static const struct malidp_layer malidp500_layers[] = {
@@ -588,6 +597,8 @@ static int malidp550_rotmem_required(struct malidp_hw_device *hwdev, u16 w, u16 
 	case DRM_FORMAT_BGRX8888:
 	case DRM_FORMAT_RGB888:
 	case DRM_FORMAT_BGR888:
+	case DRM_FORMAT_XYUV8888:
+	case DRM_FORMAT_XVYU2101010:
 	/* 16 lines at 2 bytes per pixel */
 	case DRM_FORMAT_RGBA5551:
 	case DRM_FORMAT_ABGR1555:
@@ -595,12 +606,18 @@ static int malidp550_rotmem_required(struct malidp_hw_device *hwdev, u16 w, u16 
 	case DRM_FORMAT_BGR565:
 	case DRM_FORMAT_UYVY:
 	case DRM_FORMAT_YUYV:
+	case DRM_FORMAT_X0L0:
+	case DRM_FORMAT_X0L2:
 		bytes_per_col = 32;
 		break;
 	/* 16 lines at 1.5 bytes per pixel */
 	case DRM_FORMAT_NV12:
 	case DRM_FORMAT_YUV420:
 		bytes_per_col = 24;
+		break;
+	/* 16 lines at 3 bytes per pixel */
+	case DRM_FORMAT_P010:
+		bytes_per_col = 48;
 		break;
 	default:
 		return -EINVAL;
@@ -860,8 +877,8 @@ const struct malidp_hw malidp_device[MALIDP_MAX_DEVICES] = {
 					    MALIDP550_DC_IRQ_SE,
 				.vsync_irq = MALIDP550_DC_IRQ_CONF_VALID,
 			},
-			.pixel_formats = malidp550_de_formats,
-			.n_pixel_formats = ARRAY_SIZE(malidp550_de_formats),
+			.pixel_formats = malidp650_de_formats,
+			.n_pixel_formats = ARRAY_SIZE(malidp650_de_formats),
 			.bus_align_bytes = 16,
 		},
 		.query_hw = malidp650_query_hw,
