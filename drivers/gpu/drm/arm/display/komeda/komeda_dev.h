ab89073dca7eceda23507f88d5072ef1c3034452
Mihail Atanassov <mihail.atanassov@arm.com>
2019-09-27 15:55:06 +0100

    DISPLAYSW-3206: drm/komeda: Add debugfs node to control error verbosity
    Change-Id: I819db806587b3c6752c8ace1d6f7d5bc247c8b39
    Signed-off-by: Mihail Atanassov <mihail.atanassov@arm.com>

--- drivers/gpu/drm/arm/display/komeda/komeda_dev.h
+++ drivers/gpu/drm/arm/display/komeda/komeda_dev.h
@@ -223,6 +223,14 @@ struct komeda_dev {
 
 	/** @debugfs_root: root directory of komeda debugfs */
 	struct dentry *debugfs_root;
+	/**
+	 * @err_verbosity: bitmask for how much extra info to print on error
+	 *
+	 * See KOMEDA_DEV_* macros for details.
+	 */
+	u16 err_verbosity;
+	/* Print a single line per error per frame with error events. */
+#define KOMEDA_DEV_PRINT_ERR_EVENTS BIT(0)
 };
 
 static inline bool
@@ -246,9 +254,9 @@ int komeda_dev_resume(struct komeda_dev *mdev);
 int komeda_dev_suspend(struct komeda_dev *mdev);
 
 #ifdef CONFIG_DRM_KOMEDA_ERROR_PRINT
-void komeda_print_events(struct komeda_events *evts);
+void komeda_print_events(struct komeda_events *evts, struct drm_device *dev);
 #else
-static inline void komeda_print_events(struct komeda_events *evts) {}
+static inline void komeda_print_events(struct komeda_events *evts, struct drm_device *dev) {}
 #endif
 void komeda_dev_init_ad(struct komeda_dev *mdev);
 
