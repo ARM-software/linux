df03acd6a13b95b7a8486dbcb632aa6f442b0a26
James Qian Wang (Arm Technology China) <james.qian.wang@arm.com>
2019-10-15 10:41:10 +0800

    DISPLAYSW-3213: drm/komeda: Del komeda referenced legacy coproc-srv interface
    The coproc-srv will be updated to use the latest ad_coprocessor
    interface, so del the old API referenced in komeda.
    
    Change-Id: Id48a09d8a2b10d31bf4b044bff0ba56b8e85eb0f
    Signed-off-by: James Qian Wang (Arm Technology China) <james.qian.wang@arm.com>

--- drivers/gpu/drm/arm/display/komeda/komeda_crtc.c
+++ drivers/gpu/drm/arm/display/komeda/komeda_crtc.c
@@ -148,12 +148,6 @@ komeda_crtc_prepare(struct komeda_crtc *kcrtc)
 		DRM_ERROR("failed to enable pxl clk for pipe%d.\n", master->id);
 
 	if (kcrtc_st->en_coproc) {
-		err = komeda_coproc_enable(master, mode);
-		if (err) {
-			DRM_ERROR("failed to enable co-processor for pipe%d.\n", master->id);
-			kcrtc_st->en_coproc = false;
-		}
-
 		err = komeda_ad_enable(master, &kcrtc_st->base.adjusted_mode);
 		if (err) {
 			DRM_ERROR("failed to enable AD for pipe%d.\n", master->id);
@@ -194,7 +188,6 @@ komeda_crtc_unprepare(struct komeda_crtc *kcrtc)
 	mdev->dpmode = new_mode;
 
 	komeda_ad_disable(master);
-	komeda_coproc_disable(master);
 
 	clk_disable_unprepare(master->pxlclk);
 	if (new_mode == KOMEDA_MODE_INACTIVE)
