cf9b90b0d0f65899af0c203b75cb13ea88fc9272
James Qian Wang (Arm Technology China) <james.qian.wang@arm.com>
2019-10-15 10:46:50 +0800

    DISPLAYSW-3213: drm/komeda: Move coproc.h from include to co-processor folder
    Change-Id: I32d9cd9b7b68b3425c62b4a4d714c908b7f20a15
    Signed-off-by: James Qian Wang (Arm Technology China) <james.qian.wang@arm.com>

--- drivers/gpu/drm/arm/display/co-processor/coproc.h
+++ drivers/gpu/drm/arm/display/co-processor/coproc.h
@@ -1,17 +1,4 @@
-/*
- *
- * (C) COPYRIGHT 2019 ARM Limited. All rights reserved.
- *
- * This program is free software and is provided to you under the terms of the
- * GNU General Public License version 2 as published by the Free Software
- * Foundation, and any use by you of this program is subject to the terms
- * of such GNU licence.
- *
- * A copy of the licence is included with the program, and can also be obtained
- * from Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA  02110-1301, USA.
- *
- */
+/* SPDX-License-Identifier: GPL-2.0 */
 
 #ifndef _COPROC_H_
 #define _COPROC_H_
