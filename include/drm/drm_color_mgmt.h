0c0e6d3443d76ea94ccd6752d8ea388c7ec481f7
james qian wang (Arm Technology China) <james.qian.wang@arm.com>
2019-10-23 06:42:55 +0000

    drm: Add a new helper drm_color_ctm_s31_32_to_qm_n()
    Add a new helper function drm_color_ctm_s31_32_to_qm_n() for driver to
    convert S31.32 sign-magnitude to Qm.n 2's complement that supported by
    hardware.
    
    V4: Address Mihai, Daniel and Ilia's review comments.
    V5: Includes the sign bit in the value of m (Qm.n).
    V6: Allows m = 0 according to Mihail's comments.
    V6: Address Mihail's comments.
    
    Change-Id: I3cc7427247ee3c755a563d735e0fe70abdfb4c20
    Signed-off-by: james qian wang (Arm Technology China) <james.qian.wang@arm.com>
    Reviewed-by: Mihail Atanassov <mihail.atanassov@arm.com>
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    (cherry picked from commit 6d1fb6d991f18555913d45bba124a3d142d15e7e)

--- include/drm/drm_color_mgmt.h
+++ include/drm/drm_color_mgmt.h
@@ -31,6 +31,8 @@ struct drm_plane;
 
 uint32_t drm_color_lut_extract(uint32_t user_input, uint32_t bit_precision);
 u64 drm_color_lut_extract_ext(u64 user_input, u32 bit_precision);
+uint64_t drm_color_ctm_s31_32_to_qm_n(uint64_t user_input,
+				      uint32_t m, uint32_t n);
 
 void drm_crtc_enable_color_mgmt(struct drm_crtc *crtc,
 				uint degamma_lut_size,
